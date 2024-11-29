385f87c02710b3566a8fc2c2606da2124ed68430
kevsommer <kevsommer96@gmail.com>
2024-11-29 17:50:43 +0100

    implements mouse dragging

--- graphics/ViewportTransformer.py
+++ graphics/ViewportTransformer.py
@@ -8,15 +8,21 @@ class ViewportTransformer:
     def __init__(self):
         self.zoom = 1
         self.zoom_factor = 0.1
+        self.translate_x = 0
+        self.translate_y = 0
     
     def zoom_in(self):
         self.zoom = min(self.zoom + self.zoom_factor, MAX_ZOOM)
     
     def zoom_out(self):
         self.zoom = max(self.zoom - self.zoom_factor, MIN_ZOOM)
+    
+    def move(self, dx, dy):
+        self.translate_x += dx
+        self.translate_y += dy
 
     def transform_to_screen_coords(self, x, y):
-        return x * self.zoom + WINDOW_OFFSET_X, y * self.zoom + WINDOW_OFFSET_Y
+        return (x * self.zoom + WINDOW_OFFSET_X + self.translate_x), (y * self.zoom + WINDOW_OFFSET_Y + self.translate_y)
     
     def transform_to_world_coords(self, x, y):
-        return (x - WINDOW_OFFSET_X) / self.zoom, (y - WINDOW_OFFSET_Y) / self.zoom
+        return (x - self.translate_x - WINDOW_OFFSET_X) / self.zoom, (y - self.translate_y - WINDOW_OFFSET_Y) / self.zoom
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class Game:
         self.map_state_manager = MapStateManager(self.hex_tile_map)
         self.rendering_manager = RenderingManager()
         self.viewport_transformer = ViewportTransformer()
+        self.dragging = False
 
     def run(self):
         while self.running:
@@ -21,16 +22,36 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
                     pygame.quit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.viewport_transformer.zoom_in()
-                    if event.key == pygame.K_DOWN:
-                        self.viewport_transformer.zoom_out()
-        
+
+                self.handle_zoom(event)
+                self.handle_mouse(event)
+
             self.screen.fill(COLORS['black'])
             self.update()
             self.draw()
             pygame.display.flip()
+
+    def handle_zoom(self, event): 
+        if event.type == pygame.MOUSEWHEEL:
+            if event.y < 0: 
+                self.viewport_transformer.zoom_out()
+            else:
+                self.viewport_transformer.zoom_in()
+
+    def handle_mouse(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not self.dragging:
+            self.dragging = True
+            self.drag_start_pos = pygame.mouse.get_pos()
+
+        if event.type == pygame.MOUSEBUTTONUP and self.dragging and event.button == 1: # Left mouse button
+            self.dragging = False
+
+        if event.type == pygame.MOUSEMOTION and self.dragging   :
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            dx = mouse_x - self.drag_start_pos[0]
+            dy = mouse_y - self.drag_start_pos[1]
+            self.viewport_transformer.move(dx, dy)
+            self.drag_start_pos = (mouse_x, mouse_y)
 
     def draw(self):
         self.rendering_manager.draw_hex_map(self.map_state_manager, self.hex_tile_map, self.viewport_transformer, self.screen)
