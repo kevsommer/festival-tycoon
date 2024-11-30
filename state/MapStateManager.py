9195143a07db45df7d48d366263b18d333e0471f
kevsommer <kevsommer96@gmail.com>
2024-11-30 01:30:59 +0100

    refactors MapStateManager

--- state/MapStateManager.py
+++ state/MapStateManager.py
@@ -2,28 +2,34 @@ from .TileState import TileState, TileId
 from .StageTile import StageTile
 from core.AxialHexCoord import AxialHexCoord
 
+# generate all hex axials within a certain radius of a hexagon
+def gen_hex_axials(N: int) -> list[AxialHexCoord]:
+    hex_axials = []
+    for q in range(-N, N+1):
+        for r in range(max(-N, -q-N), min(N, -q+N)+1):
+            hex_axials.append((q, r))
+    
+    return list(map(lambda axial: AxialHexCoord(axial[0], axial[1]), hex_axials))
+
+def initialise_tile_states() -> dict[TileId, TileState]:
+    hex_axials = gen_hex_axials(6)
+    
+    tile_states: dict[TileId, TileState] = {}
+    
+    for coord in hex_axials:
+        tile_id = (coord.q, coord.r)
+        if tile_id == (0, 0):
+            tile_states[tile_id] = StageTile(tile_id, coord)
+        else:
+            tile_states[tile_id] = TileState(tile_id, coord)
+    
+    return tile_states
+
 class MapStateManager:
     def __init__(self):
-        hex_axials = self.gen_hex_axials(6)
-        
-        self.tile_states: dict[TileId, TileState] = {}
+        self.tile_states = initialise_tile_states()
         self.selected_tile = None
         
-        for coord in hex_axials:
-            tile_id = (coord.q, coord.r)
-            self.tile_states[tile_id] = TileState(tile_id, coord)
-
-        self.tile_states[(0, 0)] = StageTile((0, 0), AxialHexCoord(0, 0))
-        
-    # generate all hex axials within a certain radius of a hexagon
-    def gen_hex_axials(self, N):
-        hex_axials = []
-        for q in range(-N, N+1):
-            for r in range(max(-N, -q-N), min(N, -q+N)+1):
-                hex_axials.append((q, r))
-        
-        return list(map(lambda axial: AxialHexCoord(axial[0], axial[1]), hex_axials))
-                    
     def handle_tile_click(self, tile_id: TileId):
         self.selected_tile = tile_id
 
